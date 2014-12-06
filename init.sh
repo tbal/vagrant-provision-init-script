6a295b4cec32c41925e84fc722f19c2c9150e0a4
Tilo Baller <tilo.baller@gmail.com>
2014-12-06 15:06:44 +0100

    Support file names and aliases with spaces as argument

--- init.sh
+++ init.sh
@@ -10,6 +10,7 @@ SCRIPT_COLLECTION_GIT_REMOTE="https://github.com/tbal/vagrant-provision-script-c
 SCRIPT_COLLECTION_GIT_CLONE_ARGS="-b master --depth 1"
 SCRIPT_COLLECTION_PATH="/tmp/vagrant-provision-script-collection"
 SEARCH_PATHS=".:/vagrant:$SCRIPT_COLLECTION_PATH"
+STRING_FILE_LIST_SEPARATOR=":"
 # <<< CONFIG
 
 
@@ -30,8 +31,8 @@ set -o nounset
 # FUNCTION DEFINITIONS
 ##
 cleanup() {
-    echo "### Cleaning up ###"
-    rm -rf $SCRIPT_COLLECTION_PATH
+    echo "##### Cleaning up #####"
+    rm -rf "$SCRIPT_COLLECTION_PATH"
 }
 
 
@@ -39,60 +40,78 @@ cleanup() {
 ###
 # MAIN SCRIPT
 ##
+
 echo "##### PROVISION INIT SCRIPT ($VERSION_TAG) #####"
+
 
 
 # install git
 if [ ! `which git` ]; then
-    echo "### Installing git ###"
+    echo "##### Installing git #####"
     apt-get install -qq "$GIT_PACKAGE_NAME"
 fi
 
 
+
 # download script collection
 if [ ! -d $SCRIPT_COLLECTION_PATH ]; then
-    echo "### Downloading script collection ###"
-    git clone $SCRIPT_COLLECTION_GIT_CLONE_ARGS $SCRIPT_COLLECTION_GIT_REMOTE "$SCRIPT_COLLECTION_PATH"
+    echo "##### Downloading script collection #####"
+    git clone $SCRIPT_COLLECTION_GIT_CLONE_ARGS "$SCRIPT_COLLECTION_GIT_REMOTE" "$SCRIPT_COLLECTION_PATH"
 fi
 
 
+
 # validate if all arguments are either valid local files or script collection aliases
-echo "### Validating arguments ###"
+echo "##### Validating arguments #####"
+
 SCRIPTS=
-for ARG in $@; do
+for ARG in "$@"; do
 
     # argument is a local file in one of the search pathes (first match will be used)
-    for SEARCH_PATH in `echo $SEARCH_PATHS | tr : ' '`; do
+    while read SEARCH_PATH; do
+
         # transform relative paths to absolute paths
         # TODO: check first character == "." and replace it with current absolute path
-        [ "$SEARCH_PATH" == "." ] && SEARCH_PATH=$(dirname `readlink -f "$0"`)
+        [ "$SEARCH_PATH" == "." ] && SEARCH_PATH="$(dirname `readlink -f "$0"`)"
 
         if [ -f "$SEARCH_PATH/$ARG" ]; then
-            SCRIPTS="$SCRIPTS $SEARCH_PATH/$ARG"
+            SCRIPTS="${SCRIPTS}${STRING_FILE_LIST_SEPARATOR}${SEARCH_PATH}/$ARG"
             continue 2;
         fi
-    done
+
+    done < <(echo "$SEARCH_PATHS" | tr $STRING_FILE_LIST_SEPARATOR "\n")
+
 
     # argument is an alias for a file of the script collection
     if [ -f "$SCRIPT_COLLECTION_PATH/$ARG.sh" ]; then
-        SCRIPTS="$SCRIPTS $SCRIPT_COLLECTION_PATH/$ARG.sh"
+        SCRIPTS="${SCRIPTS}${STRING_FILE_LIST_SEPARATOR}${SCRIPT_COLLECTION_PATH}/$ARG.sh"
         continue;
     fi
 
+
     # neither local file, nor alias => invalid argument
     echo "ERROR: Invalid argument: $ARG"
-    echo "### Aborting ###"
+    echo "##### Aborting #####"
     cleanup
     exit
 
 done
 
 
+
+# remove first character, which is a separator used for concatenation
+SCRIPTS=${SCRIPTS:1}
+
+
+
 # execute scripts
-for SCRIPT in $SCRIPTS; do
-    echo "### Executing script: $SCRIPT ###"
-    /bin/chmod +x $SCRIPT && . $SCRIPT
-done
+while read SCRIPT; do
+
+    echo "##### Executing script: $SCRIPT #####"
+    /bin/chmod +x "$SCRIPT" && . "$SCRIPT"
+
+done < <(echo "$SCRIPTS" | tr $STRING_FILE_LIST_SEPARATOR "\n")
+
 
 
 # cleanup temporary files
